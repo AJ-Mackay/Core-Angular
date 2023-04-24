0739434c616678d5aa5325021aa73883c93c57fb
Paul Mackay <paul.m.mackay85@gmail.com>
2023-04-24 16:50:36 +0100

    Getting the data from the confirmation modal

--- client/src/app/_services/confirm.service.ts
+++ client/src/app/_services/confirm.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { ConfirmDialogComponent } from '../modals/confirm-dialog/confirm-dialog.component';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,16 @@ export class ConfirmService {
 
   constructor(private modalService: BsModalService) { }
 
-  confirm(title = 'Confirmation', message = 'Are you sure you want to do this?', btnOkText = 'Ok', btnCancelText = 'Cancel') {
+  confirm(title = 'Confirmation', message = 'Are you sure you want to do this?', btnOkText = 'Ok', btnCancelText = 'Cancel'): Observable<boolean> {
     const config = {
       initialState: {
         title, message, btnOkText, btnCancelText
       }
     }
-    this.bsModalRef = this.modalService.show(ConfirmDialogComponent, config)
+    this.bsModalRef = this.modalService.show(ConfirmDialogComponent, config);
+    return this.bsModalRef.onHidden!.pipe(map(() => {
+      return this.bsModalRef!.content!.result
+    })
+    )
   }
 }
